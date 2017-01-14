e730273dea51ec2cbe7f530a6a7b0bf133553e3e
Nicholas Yang <nick@nicholasyang.com>
2017-01-13 23:56:24 -0500

    fixed add user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   def User.admin_new(user_params)
-    user_params['is_admin?'] = user_params['name'] == 'nicholas'
+    user_params['is_admin'] = user_params['name'] == 'nicholas'
     new(user_params)
   end
 end
