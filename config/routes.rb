50b0145c71666be6cc591871831bbaf50e423311
Nicholas Yang <nick@nicholasyang.com>
2017-01-17 17:36:07 -0500

    welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   resources :sessions
   get 'articles/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'articles#index'
+  root 'welcome#index'
 end
