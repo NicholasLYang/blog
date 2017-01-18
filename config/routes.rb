576ea8d73af951b2df0a676502c6ac236fad0c46
Nicholas Yang <nick@nicholasyang.com>
2017-01-18 00:11:23 -0500

    split header and user info

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources  :articles
   resources :sessions
   get 'articles/index'
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'articles#index'
 end
