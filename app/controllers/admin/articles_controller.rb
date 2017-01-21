412f424ca2237425bd0e7c23afeb907b96b6e9f9
Nicholas Yang <nick@nicholasyang.com>
2017-01-21 00:32:37 -0500

    no need for authorization to see about page

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -7,8 +7,11 @@ class Admin::ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def update
