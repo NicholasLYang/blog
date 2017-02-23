7c629ecf8099e81533df33f0919f0f04b2e2bfc8
Nicholas Yang <nick@nicholasyang.com>
2017-02-22 23:33:27 -0500

    whoops now it works

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ set :repo_url, "git@github.com:NicholasLYang/blog.git"
 
 set :deploy_to, '/home/deploy/blog'
 
+
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
