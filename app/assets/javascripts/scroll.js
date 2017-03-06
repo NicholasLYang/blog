2a677e08304d6b0299514c6f4ad67fd42c08712f
Nicholas Yang <nick@nicholasyang.com>
2017-03-06 00:13:43 -0500

    added scroll function for alert, fixed tags on editing

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,15 +1,17 @@
 is_not_mobile = window.matchMedia("(min-width: 600px)").matches
 
-$(window).scroll(function() {
+$(window).scroll(function scrollFunc() {
     if (is_not_mobile) {
         if ($(document).scrollTop() > 50) {
             console.log("Sliding up");
             $('.header-bar').slideUp();
+            $('.alert-bar').slideUp();
             $('.user-bar').slideUp();
         }
         else{
             console.log("Sliding down");
             $('.header-bar').slideDown();
+            $('.alert-bar').slideUp();
             $('.user-bar').slideDown();
         }
     }
