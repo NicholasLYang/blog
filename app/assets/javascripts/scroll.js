a5409bfbf0673d25f2d3e9d2028bee9872a4eb48
Nicholas Yang <nick@nicholasyang.com>
2017-03-05 22:17:08 -0500

    turned off animation for mobile

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,10 +1,16 @@
+is_not_mobile = window.matchMedia("(min-width: 600px)").matches
+
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-        $('.header-bar').slideUp();
-        $('.user-bar').slideUp();
-    }
-    else{
-        $('.header-bar').slideDown();
-        $('.user-bar').slideDown();
+    if (is_not_mobile) {
+        if ($(document).scrollTop() > 50) {
+            console.log("Sliding up");
+            $('.header-bar').slideUp();
+            $('.user-bar').slideUp();
+        }
+        else{
+            console.log("Sliding down");
+            $('.header-bar').slideDown();
+            $('.user-bar').slideDown();
+        }
     }
 });
