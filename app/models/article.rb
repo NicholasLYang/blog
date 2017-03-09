fe5939a0827037372b689f7f1812c3c945e0f980
Nicholas Yang <nick@nicholasyang.com>
2017-03-09 15:38:02 -0500

    added vanity urls

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,15 @@ class Article < ApplicationRecord
   has_many :comments
   validates :title, presence: true,
             length: {minimum: 5}
+
+  def slug
+    # Slug is the part of a URL that identifies a page
+    # in human readable keywords
+    title.downcase.gsub(" ", "-")
+  end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
+
 end
