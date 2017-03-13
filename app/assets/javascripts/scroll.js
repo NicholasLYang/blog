367d92f8f620b7a48180fb8c67cdead8bf91e7d3
Nicholas Yang <nick@nicholasyang.com>
2017-03-12 23:27:44 -0400

    trying out something different

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,18 +1,35 @@
 is_not_mobile = window.matchMedia("(min-width: 600px)").matches
 
-$(window).scroll(function scrollFunc() {
-    if (is_not_mobile) {
-        if ($(document).scrollTop() > 50) {
-            console.log("Sliding up");
-            $('.header-bar').slideUp();
-            $('.alert-bar').slideUp();
-            $('.user-bar').slideUp();
-        }
-        else{
-            console.log("Sliding down");
-            $('.header-bar').slideDown();
-            $('.alert-bar').slideUp();
-            $('.user-bar').slideDown();
-        }
+// $(window).scroll(function scrollFunc() {
+//     if (is_not_mobile) {
+//         if ($(document).scrollTop() > 50) {
+//             console.log("Sliding up");
+//             $('.header-bar').slideUp();
+//             $('.alert-bar').slideUp();
+//             $('.user-bar').slideUp();
+//         }
+//         else{
+//             console.log("Sliding down");
+//             $('.header-bar').slideDown();
+//             $('.alert-bar').slideUp();
+//             $('.user-bar').slideDown();
+//         }
+//     }
+// });
+
+
+
+$(window).scroll(function () {
+    var header = $('.header-bar');
+    var user = $('.user-bar');
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > 100) {
+        header.addClass("header-anchored");
+        user.addClass("user-anchored");
+    }
+    else {
+        header.removeClass("header-anchored");
+        user.removeClass("user-anchored");
     }
 });
