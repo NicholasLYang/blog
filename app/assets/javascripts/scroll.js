6a7d0a350478869fb39601df7865893678c6c2c9
Nicholas Yang <nick@nicholasyang.com>
2017-03-14 18:40:58 -0400

    tweaking animation speed

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -3,10 +3,14 @@ is_not_mobile = window.matchMedia("(min-width: 600px)").matches
 $(window).scroll(function scrollFunc() {
     if (is_not_mobile) {
         if ($(document).scrollTop() > 50) {
-            $('.header-bar').switchClass("max-header-bar", "min-header-bar", "1000");
+            $('.header-bar').switchClass("max-header-bar",
+                                         "min-header-bar",
+                                         200);
         }
         else{
-            $('.header-bar').switchClass("min-header-bar", "max-header-bar", "1000");
+            $('.header-bar').switchClass("min-header-bar",
+                                         "max-header-bar",
+                                         200);
         }
     }
 });
