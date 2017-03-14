3a541843d0e7c23372e3bc150426275d57d6ed7c
Nicholas Yang <nick@nicholasyang.com>
2017-03-14 18:12:57 -0400

    attempting new ui

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,35 +1,15 @@
 is_not_mobile = window.matchMedia("(min-width: 600px)").matches
 
-// $(window).scroll(function scrollFunc() {
-//     if (is_not_mobile) {
-//         if ($(document).scrollTop() > 50) {
-//             console.log("Sliding up");
-//             $('.header-bar').slideUp();
-//             $('.alert-bar').slideUp();
-//             $('.user-bar').slideUp();
-//         }
-//         else{
-//             console.log("Sliding down");
-//             $('.header-bar').slideDown();
-//             $('.alert-bar').slideUp();
-//             $('.user-bar').slideDown();
-//         }
-//     }
-// });
-
-
-
-$(window).scroll(function () {
-    var header = $('.header-bar');
-    var user = $('.user-bar');
-    var scrollTop = $(window).scrollTop();
-
-    if (scrollTop > 100) {
-        header.addClass("header-anchored");
-        user.addClass("user-anchored");
-    }
-    else {
-        header.removeClass("header-anchored");
-        user.removeClass("user-anchored");
+$(window).scroll(function scrollFunc() {
+    if (is_not_mobile) {
+        if ($(document).scrollTop() > 50) {
+            $('.header-bar').switchClass("max-header-bar", "min-header-bar", "1000");
+        }
+        else{
+            $('.header-bar').switchClass("min-header-bar", "max-header-bar", "1000");
+        }
     }
 });
+
+
+
