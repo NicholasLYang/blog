cc28af95297a18919d1e8eb6c790bae8a5621cd9
Nicholas Yang <nick@nicholasyang.com>
2017-03-18 14:57:15 -0400

    fixed deleting articles bug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   acts_as_taggable
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true,
             length: {minimum: 5}
 
@@ -16,7 +16,7 @@ class Article < ApplicationRecord
     safe_title = title.downcase
     substitutions.each { |sub| safe_title.gsub!(sub[0],
                                                 sub[1]) }
-    return safe_title.parameterize 
+    return safe_title.parameterize
   end
 
   def to_param
